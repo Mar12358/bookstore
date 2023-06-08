01e5930e40fc339f4b73792909bd3a1f7331fda5
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-08 13:50:25 -0300

    Fix jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { test, expect } from 'jest';
 
 test('renders learn react link', () => {
   render(<App />);

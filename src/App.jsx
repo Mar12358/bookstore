d3fe2e262d9c1e5b2aad77bde0a90859cf681370
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-08 14:49:25 -0300

    Add Layout and Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import Home from './routes/Home';
 import Books from './routes/Books';
 // import { useState } from 'react'
 import './App.css'
+import NotMatch from './routes/NotMatch';
+import Layout from './components/Layout';
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="books" element={<Books />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="books" element={<Books />} />
+          <Route path="*" element={<NotMatch />} />
+        </Route>
      </Routes>
   )
 }
--- src/routes/Books.jsx
+++ src/routes/Books.jsx
@@ -1,14 +1,10 @@
 import BookFormCreator from '../components/BookFormCreator';
 import BooksList from '../components/BooksList';
-import Navbar from '../components/Navbar';
 const Books = () => (
-  <div className="wrapper">
-    <Navbar />
-    <div className="text-wraper">
-      
-      <BooksList />
-      <BookFormCreator />
-    </div>
+  <div className="text-wraper">
+    
+    <BooksList />
+    <BookFormCreator />
   </div>
 );
 export default Books;

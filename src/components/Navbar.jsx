2862a7454774e648286a88ae8e85e0ec1e081e29
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-08 14:16:09 -0300

    Add navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { NavLink } from 'react-router-dom';
+
+const links = [
+  { path: '/', text: 'Home' },
+  { path: '/books', text: 'Books' },
+];
+const Navbar = () => (
+  <nav className="navbar">
+    <h1>Bookstore</h1>
+    <ul>
+      {links.map((link) => (
+        <li key={link.text}>
+          <NavLink to={link.path}>{link.text}</NavLink>
+        </li>
+      ))}
+    </ul>
+  </nav>
+);
+export default Navbar;

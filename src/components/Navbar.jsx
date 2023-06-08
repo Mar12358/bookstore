1aa1b20fed5f9ce1369069ee9b21b98b594f10ff
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-08 14:36:43 -0300

    Add categories to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,11 @@ import { NavLink } from 'react-router-dom';
 const links = [
   { path: '/', text: 'Home' },
   { path: '/books', text: 'Books' },
+  { path: '/categories', text: 'Categories' },
 ];
 const Navbar = () => (
   <nav className="navbar">
-    <h1>Bookstore</h1>
+    <h1>Bookstore CMS</h1>
     <ul>
       {links.map((link) => (
         <li key={link.text}>

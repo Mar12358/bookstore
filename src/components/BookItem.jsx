3d1ced74bcc0ff4d96b0ad0c5a701587821180cb
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-09 10:58:00 -0300

    Render book

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -1,9 +1,18 @@
-const BookItem = () => {
+import PropTypes from 'prop-types';
+
+const BookItem = ({ book }) => {
   return (
-    <>
+    <div className="book-item">
+      <h3>{book.title}</h3>
+      <p>{book.category}</p>
       <button>Remove</button>
-    </>
+    </div>
   );
 };
-
+BookItem.propTypes = {
+  book: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  }).isRequired,
+};
 export default BookItem;

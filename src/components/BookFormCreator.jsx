d422b90f87fb672dd77ceebb9e7368620fedaa0d
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-09 10:19:55 -0300

    Add form

--- src/components/BookFormCreator.jsx
+++ src/components/BookFormCreator.jsx
@@ -1,8 +1,15 @@
 const BookFormCreator = () => {
   return (
-    <>
-      <div>Book Form Creator Here</div>
-    </>
+    <form>
+      <input type="text" name="title" placeholder="Book title" />
+      <select name="category" placeholder="Category">
+        <option value="fiction">Fiction</option>
+        <option value="non-fiction">Non-fiction</option>
+        <option value="sci-fi">Science Fiction</option>
+        <option value="fantasy">Fantasy</option>
+      </select>
+      <button type="button" name="add"> Add </button>
+    </form>
   );
 };
 

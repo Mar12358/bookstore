6f5c586a5b441b6bad8ce0dafff734aed414289a
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-09 10:58:37 -0300

    Recieve dummy book list and pass it as prop

--- src/components/BooksList.jsx
+++ src/components/BooksList.jsx
@@ -1,10 +1,33 @@
-import BookController from "./BookController";
+import BookItem from "./BookItem";
 
 const BooksList = () => {
+  const books = [
+    {
+      id: "1",
+      title: "Book 1",
+      category: "Fiction"
+    },
+    {
+      id: "2",
+      title: "Book 2",
+      category: "Non-fiction"
+    },
+    {
+      id: "3",
+      title: "Book 3",
+      category: "Science Fiction"
+    },
+    {
+      id: "4",
+      title: "Book 4",
+      category: "Fantasy"
+    }
+  ];
   return (
     <>
-      <div>Book List Here</div>
-      <BookController />
+      {books.map((book) => (
+        <BookItem key={book.id} book={book} />
+      ))}
     </>
   );
 };

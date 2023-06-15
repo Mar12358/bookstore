69d3d0e3d169c7d94c9d1a817a05307b7582c8df
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-15 02:26:19 -0300

    Add author key

--- src/components/BookFormCreator.jsx
+++ src/components/BookFormCreator.jsx
@@ -6,11 +6,17 @@ const BookFormCreator = () => {
   const dispatch = useDispatch();
   const [id, setId] = useState('');
   const [title, setTitle] = useState(''); // update state to see title
+  const [author, setAuthor] = useState(''); // update state to see author
   const [category, setCategory] = useState(''); // state for category value
   const books = useSelector(state => state.books.books);
   console.log(books)
+
   const handleTitleChange = (e) => {
     setTitle(e.target.value); // update title state when input changes
+  };
+
+  const handleAuthorChange = (e) => {
+    setAuthor(e.target.value); // update title state when input changes
   };
 
   const handleCategoryChange = (e) => {
@@ -21,11 +27,12 @@ const BookFormCreator = () => {
     e.preventDefault();
     setId((books.length + 1).toString());
     console.log(title, category);
-    dispatch(add({ id, title, category })); // call "add" action passin title and category as argument
+    dispatch(add({ id, title, author, category })); // call "add" action passin title and category as argument
   };
   return (
     <form onSubmit={handleSubmit}>
       <input type="text" name="title" placeholder="Book title" value={title} onChange={handleTitleChange}/>
+      <input type="text" name="author" placeholder="Book author" value={author} onChange={handleAuthorChange}/>
       <select name="category" placeholder="Category" value={category} onChange={handleCategoryChange}>
         <option value="fiction">Fiction</option>
         <option value="non-fiction">Non-fiction</option>

bb5d6f71656a3604dacd26851fe418e0c15b0d46
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-17 00:07:42 -0300

    Delete import App.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import Books from './routes/Books';
 // import { useState } from 'react'
-import './App.css'
 import NotMatch from './routes/NotMatch';
 import Layout from './components/Layout';
 

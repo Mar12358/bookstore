e62d3d1bf3c9617500b60514ae4b727bfe7c5d90
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-17 01:39:19 -0300

    Delete unused comented lines

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import Books from './routes/Books';
-// import { useState } from 'react'
 import NotMatch from './routes/NotMatch';
 import Layout from './components/Layout';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
       <Routes>
         <Route path="/" element={<Layout />}>
